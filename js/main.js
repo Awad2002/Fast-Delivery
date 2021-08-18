190a9df25a1103c0181416ef0ac3fc1dceb25aab
Awad2002 <awadamr33@gmail.com>
2021-08-18 12:39:21 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    stagePadding: 372,
+    loop:true,
+    margin:27,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
