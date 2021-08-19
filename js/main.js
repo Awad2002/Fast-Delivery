d9850adafede251ef00cfe09368e70e848cdd71c
Awad2002 <awadamr33@gmail.com>
2021-08-19 11:43:54 +0300

    index-resonsive

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
+
 $('.owl-carousel').owlCarousel({
     stagePadding: 372,
     loop:true,
-    margin:27,
-    nav:true,
+    margin:30,
+    responsiveClass:true,
     responsive:{
         0:{
-            items:1
+            items:1,
+            stagePadding: 10,
         },
         600:{
-            items:1
+            items:3,
+            stagePadding: 30,
         },
         1000:{
-            items:1
+            items:5,
         }
     }
 })
-
